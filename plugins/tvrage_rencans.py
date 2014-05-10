ebe19686bbdf32d6ed404734f03d81efed2f9d14
tarzasai <ggelardi@gmail.com>
2014-05-10 16:51:32 +0200

    .

--- plugins/tvrage_rencans.py
+++ plugins/tvrage_rencans.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals, division, absolute_import
 import logging
 import time
+import re
+import string
+import random
 
 from datetime import datetime
 
@@ -17,15 +20,25 @@ class TVRageRenCans(object):
     """
     """
     
-    schema = {'type': 'integer'}
+    schema = {
+        'type': 'object',
+        'properties': {
+            'pages': {'type': 'integer'},
+            'context': {'enum': ['renewed', 'canceled']}
+        },
+        'required': ['pages', 'context'],
+        'additionalProperties': False
+    }
     
     def on_task_input(self, task, config):
         if not config:
             return
-        lines = {}
-        for i in range(config):
+        entries = []
+        check = []
+        for i in range(config['pages']):
             try:
-                page = task.requests.get('http://www.tvrage.com/status_update.php?start=' + str(i-1))
+                page = task.requests.get('http://www.tvrage.com/status_update.php?show_only=%d&start=%d' % 
+                                         (1 if config['context'] == 'renewed' else 2, i-1))
                 soup = get_soup(page.text)
                 trs = soup.find_all('tr', attrs={'id': 'brow'})
             except RequestException as err:
@@ -42,24 +55,37 @@ class TVRageRenCans(object):
                 '''
                 if len(l) < 4:
                     raise plugin.PluginError('Unknown item structure!')
-                #netw = l[0].text  # CBS
-                show = l[1].text  # Undercover Boss
+                netw = l[0].text  # CBS
+                text = l[1].text  # Undercover Boss
                 date = l[2].text  # Mar 13, 2014
                 stat = l[3].text  # Renewed for S06
-                text = '%s %s' % (show, stat[0].lower() + stat[1:])
-                rows = lines.setdefault(date, [])
-                if not text in rows:
-                    rows.append(text)
-        entries = []
-        for k in sorted(lines.keys()):
-            d = datetime.fromtimestamp(time.mktime(time.strptime(k, "%b %d, %Y")))
-            if d.date() < datetime.today().date(): # skip today, or we may lose something
-                entry = Entry()
-                entry['title'] = entry['url'] = 'TVRage-' + k.replace(',', '').replace(' ', '-')
-                entry['tvrsu_date'] = d
-                entry['tvrsu_lines'] = sorted(lines[k])
-                entries.append(entry)
-        return entries
+                try:
+                    show = re.sub(r'\s\(\d more eps? listed.*', '', text)
+                except:
+                    log.warning('Unrecognized text "%s", skipping...' % text)
+                    continue
+                if show.lower() in check:
+                    continue
+                try:
+                    surl = 'http://www.tvrage.com%s' % l[1].contents[0].attrs['href']
+                except:
+                    surl = None
+                # skip today news
+                d = datetime.fromtimestamp(time.mktime(time.strptime(date, "%b %d, %Y")))
+                if show and d.date() < datetime.today().date():
+                    entry = Entry()
+                    entry['title'] = re.sub(r'\s\(\d more eps? listed.*', '', show)
+                    entry['series_name'] = entry['title']
+                    entry['url'] = 'http://localhost/fake-tvrage-news/%s' % \
+                        ''.join([random.choice(string.letters + string.digits) for x in range(1, 30)])
+                    entry['tvrage_url'] = surl
+                    entry['tvrage_text'] = show
+                    entry['tvrage_date'] = d
+                    entry['tvrage_status'] = stat
+                    entry['tvrage_network'] = netw
+                    entries.append(entry)
+                    check.append(show.lower())
+        return sorted(entries, key=lambda k: k['series_name'])
 
 
 @event('plugin.register')
