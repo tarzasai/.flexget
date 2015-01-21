93935d3b9274b83fe88392bb1e5b0252452a6e58
tarzasai <ggelardi@gmail.com>
2015-01-21 02:53:40 +0100

    .

--- plugins/uoccin_queue.py
+++ plugins/uoccin_queue.py
@@ -38,12 +38,13 @@ class UoccinQueue(object):
                 if not imdb_id in data:
                     self.log.verbose('adding movie %s (%s) to Uoccin collection' % (imdb_id, title))
                     data[imdb_id] = { 'name': title }
-                    if 'quality' in config:
-                        data[imdb_id]['quality'] = config['quality']
+                    q = entry.get('quality', config.get('quality', None))
+                    if q:
+                        data[imdb_id]['quality'] = q
                     n += 1
             if n > 0:
                 with open(dest, 'w') as f:
-                    json.dump(data, f)
+                    json.dump(data, f, sort_keys=True, indent=4, separators=(',', ': '))
             self.log.info('%d movies added to Uoccin watchlist' % n)
         if series:
             dest = os.path.join(config['path'], 'series.watchlist.json')
@@ -56,12 +57,13 @@ class UoccinQueue(object):
                 if not tvdb_id in data:
                     self.log.verbose('adding series %s (%s) to Uoccin watchlist' % (tvdb_id, title))
                     data[tvdb_id] = { 'name': title }
-                    if 'quality' in config:
-                        data[tvdb_id]['quality'] = config['quality']
+                    q = entry.get('quality', config.get('quality', None))
+                    if q:
+                        data[tvdb_id]['quality'] = q
                     n += 1
             if n > 0:
                 with open(dest, 'w') as f:
-                    json.dump(data, f)
+                    json.dump(data, f, sort_keys=True, indent=4, separators=(',', ': '))
             self.log.info('%d series added to Uoccin watchlist' % n)
 
 
