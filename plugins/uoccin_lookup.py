b5ad76ce960b7d95d9eefc733584dfe91e95d3bd
tarzasai <ggelardi@gmail.com>
2015-01-23 11:33:47 +0100

    .

--- plugins/uoccin_lookup.py
+++ plugins/uoccin_lookup.py
@@ -15,21 +15,31 @@ class UoccinLookup(object):
     def on_task_metainfo(self, task, config):
         if not task.entries:
             return
-        movcoll = self.get_json(config, 'movies.collected.json')
-        movseen = self.get_json(config, 'movies.watched.json')
-        sercoll = self.get_json(config, 'series.collected.json')
-        serseen = self.get_json(config, 'series.watched.json')
-        if not (movcoll or movseen or sercoll or serseen):
+        squeu = self.get_json(config, 'series.watchlist.json')
+        sseen = self.get_json(config, 'series.watched.json')
+        scoll = self.get_json(config, 'series.collected.json')
+        mqueu = self.get_json(config, 'movies.watchlist.json')
+        mseen = self.get_json(config, 'movies.watched.json')
+        mcoll = self.get_json(config, 'movies.collected.json')
+        if not (mqueu or mcoll or mseen or squeu or scoll or sseen):
             return
         for entry in task.entries:
-            if all(field in entry for field in ['tvdb_id', 'series_season', 'series_episode']):
-                eid = '%s.S%02dE%02d' % (entry['tvdb_id'], entry['series_season'], entry['series_episode'])
-                entry['uoccin_collected'] = eid in sercoll
-                entry['uoccin_watched'] = eid in serseen
+            if 'tvdb_id' in entry:
+                sid = str(entry['tvdb_id'])
+                entry['uoccin_queued'] = sid in squeu
+                if all(field in entry for field in ['series_season', 'series_episode']):
+                    sno = str(entry['series_season'])
+                    eno = entry['series_episode']
+                    entry['uoccin_watched'] = sid in sseen and sno in sseen[sid] and eno in sseen[sid][sno]
+                    entry['uoccin_collected'] = ('%s.S%02dE%02d' % (sid, entry['series_season'], eno)) in scoll
+                else:
+                    entry['uoccin_watched'] = False
+                    entry['uoccin_collected'] = False
             elif 'imdb_id' in entry:
                 eid = entry['imdb_id']
-                entry['uoccin_collected'] = eid in movcoll
-                entry['uoccin_watched'] = eid in movseen
+                entry['uoccin_queued'] = eid in mqueu
+                entry['uoccin_watched'] = eid in mseen
+                entry['uoccin_collected'] = eid in mcoll
     
     def get_json(self, config, filename):
         fn = os.path.join(config, filename)
