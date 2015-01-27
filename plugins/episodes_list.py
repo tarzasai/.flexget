cf7db332e73cc965485c89441c01d6780d301d8d
tarzasai <ggelardi@gmail.com>
2015-01-27 02:17:28 +0100

    .

--- plugins/episodes_list.py
+++ plugins/episodes_list.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals, division, absolute_import
-import logging
 
 from flexget import plugin
+from flexget.entry import Entry
 from flexget.event import event
 from flexget.plugins.filter.series import Series, Episode, SeriesTask
 
@@ -15,18 +15,24 @@ class GetEpisodesList(object):
     def on_task_input(self, task, config):
         """asd"""
         entries = []
-        
         slist = (task.session.query(Series).outerjoin(Series.episodes).outerjoin(Episode.releases).
-                 outerjoin(Series.in_tasks).group_by(Series.id).having(func.count(SeriesTask.id) >= 1).
+                 outerjoin(Series.in_tasks).group_by(Series.id).having(func.count(SeriesTask.id) < 1).
                  order_by(Series.name))
-        
         for series in slist:
-            
             elist = (task.session.query(Episode).filter(Episode.series_id == series.id).
                      order_by(Episode.season, Episode.number).all())
-            
-        
-        
+            for episode in elist:
+                self.log.debug('Found episode %s for series "%s"' % (episode.identifier, series.name))
+                entry = Entry()
+                entry['title'] = '%s %s' % (series.name, episode.identifier)
+                entry['url'] = 'http://localhost/mock/%s' % hash(entry['title'])
+                entry['series_name'] = series.name
+                entry['series_season'] = episode.season
+                entry['series_episode'] = episode.number
+                if entry.isvalid():
+                    entries.append(entry)
+                else:
+                    self.log.debug('Invalid entry created? %s' % entry)
         return entries
 
 
