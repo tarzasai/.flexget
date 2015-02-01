044ec531968890f37a561b653d7e80e2cfd1c1f0
Giorgio Gelardi <tarzasai@users.noreply.github.com>
2015-02-01 02:09:57 +0100

    Delete .copy_from_flexget.bat

--- .copy_from_flexget.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-copy /V /Y C:\Workspace\Flexget\flexget\plugins\local\*.py C:\Workspace\.flexget\plugins\
-del C:\Workspace\.flexget\plugins\__init__.py
-del C:\Workspace\.flexget\plugins\friendfeed2.py
-pause
